d48509d84a61b4c2850166aaf71db2c22c37f1ea
Kielx <kiel66@gmail.com>
2020-12-31 22:49:20 +0100

    Add cards showing total losses and income

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import MainChart from "./components/MainChart";
 import MainNavbar from "./components/MainNavbar";
 import MainCard from "./components/MainCard";
 
+import { calculateBalance } from "./Utils";
+
 function App() {
   const [parsed, setParsed] = useState("");
   const [graphData, setGraphData] = useState([]);
@@ -51,22 +53,45 @@ function App() {
   return (
     <div className="App">
       <MainNavbar parsed={parsed} setParsed={setParsed}></MainNavbar>
+      <button onClick={() => console.log(calculateBalance(parsed))}>
+        asadf
+      </button>
       <MainChart graphData={graphData}></MainChart>
       <Container fluid>
         <Row>
           <MainCard
             cardIcon={<i className="far fa-money-bill-alt fa-2x"></i>}
-            cardHeader="Income over time"
-            bg="success"
-            cardTitle="Total income in selected  time period:"
+            cardHeader="Income/Losses over time"
+            bg="primary"
+            cardTitle="Total income/losses in selected  time period:"
             cardText={
               graphData.length !== 0
-                ? graphData[graphData.length - 1]["y"] - graphData[0]["y"]
+                ? calculateBalance(parsed).balance.toFixed(2)
                 : "Insert a file first"
             }
           ></MainCard>
-          <MainCard></MainCard>
-          <MainCard></MainCard>
+          <MainCard
+            cardIcon={<i className="fas fa-arrow-down fa-2x"></i>}
+            cardHeader="Total losses"
+            bg="danger"
+            cardTitle="Total losses in selected time period:"
+            cardText={
+              graphData.length !== 0
+                ? calculateBalance(parsed).negativeBalance.toFixed(2)
+                : "Insert a file first"
+            }
+          ></MainCard>
+          <MainCard
+            cardIcon={<i className="fas fa-arrow-up fa-2x"></i>}
+            cardHeader="Total income"
+            bg="success"
+            cardTitle="Total income in selected time period:"
+            cardText={
+              graphData.length !== 0
+                ? calculateBalance(parsed).positiveBalance.toFixed(2)
+                : "Insert a file first"
+            }
+          ></MainCard>
         </Row>
       </Container>
       <form>

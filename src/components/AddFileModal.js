3f41e1d3a5d7b1ca07ca3d0c77e3167b3581453f
Kielx <kiel66@gmail.com>
2021-01-05 19:58:18 +0100

    Change navbar to be responsive and with light styling

--- src/components/AddFileModal.js
+++ src/components/AddFileModal.js
@@ -12,7 +12,7 @@ export default function AddFileModal(props) {
 
   return (
     <>
-      <Button variant="outline-light" onClick={handleShow}>
+      <Button variant="outline-primary" onClick={handleShow}>
         Choose a file to analyze
       </Button>
 
--- src/components/MainNavbar.js
+++ src/components/MainNavbar.js
@@ -1,20 +1,26 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import AddFileModal from "./AddFileModal";
+import Nav from "react-bootstrap/Nav";
 
 export default function MainNavbar(props) {
   return (
-    <Navbar bg="primary" variant="dark" className="justify-content-between">
+    <Navbar expand="lg" variant="light" bg="light">
       <Navbar.Brand href="#home">
-        <div>
-          <i className="fas fa-chart-pie fa-2x"></i>
-          {"    Expenses Analyzer"}{" "}
-        </div>
+        <i className="fas fa-chart-pie fa-2x"></i>
       </Navbar.Brand>
-      <AddFileModal
-        parsed={props.parsed}
-        setParsed={props.setParsed}
-      ></AddFileModal>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="d-none d-lg-block">
+          <h2>Expenses Analyzer</h2>
+        </Nav>
+        <Nav className="ml-auto">
+          <AddFileModal
+            parsed={props.parsed}
+            setParsed={props.setParsed}
+          ></AddFileModal>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 }

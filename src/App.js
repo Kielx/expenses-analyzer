e7b96a945d8f48fe34ec2a2b50b383146f130658
Kielx <kiel66@gmail.com>
2021-03-10 12:24:30 +0100

    Remove unnecessary imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { React, useState, useEffect } from "react";
-import { Row, Col, Container, Card, Button } from "react-bootstrap";
+import { Row, Col, Container, Card } from "react-bootstrap";
 
 import MainNavbar from "./components/MainNavbar";
 import MyResponsivePie from "./components/MyResponsivePie";
